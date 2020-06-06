1a44076c4889a3a8fd1676f4a49bb436a947c680
mfiless <65319546+mfiless@users.noreply.github.com>
2020-06-06 02:12:40 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 setup(
   name="snake", 
+  version="0.1.0",
   description="A toolkit for training reinforcement learning agents on the game snake.", 
-  url='https://github.com/mfiless/snake',
+  url='https://github.com/mfiless/snake.git',
   author='Leon Shams',
   license='MIT',
-  dependency_links=['https://github.com/pygame/pygame.git'], 
+  packages=['snake'],
+  install_requires=["pygame>=1.9.2"],
   zip_safe=False, 
   python_requires='>=3.6'
 )
