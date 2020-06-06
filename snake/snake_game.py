7c122c3338cafdc88fe74968341be4291eba061a
mfiless <65319546+mfiless@users.noreply.github.com>
2020-06-06 03:04:08 -0700

    Update snake_game.py

--- snake/snake_game.py
+++ snake/snake_game.py
@@ -38,7 +38,7 @@ class Snake:
 
 
 	def reset(self):
-		self.head_pos = [int(self.snake_size*self.size/4), int(self.snake_size*self.size/2)]
+		self.head_pos = [self.snake_size*int(self.size/4), self.snake_size*int(self.size/2)]
 		self.length = copy.copy(self.starting_length)
 		self.past_positions = deque([copy.copy(self.head_pos)], maxlen=self.length)
 
